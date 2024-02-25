8e8d71024e616cfd86ede1ee80e881913125d7a7
camarm-dev <contact@camarm.dev>
2024-02-25 15:26:06 +0100

    add: first version

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # ed-test-api
 Serve an API which reproduces the EcoleDirecte behavior without and EcoleDirecte account !
+
+## Lancer
+
+- Configurer dans `config.json`
+```json
+{
+  "static_token": true // Le token est statique alors il n'expirera jamais, sinon il faudra le regénérer toutes les 10 requêtes
+}
+```
+- Installer les dépendances
+```shell
+pip install fastapi
+```
+- Lancer le serveur
+```shell
+python3 main.py
+```
+
+- Visiter [localhost:8000](http://localhost:8000/docs)
+
+## Comment ça marche ?
+
+- Un dossier `requests/`, qui contient les définitions des requêtes et leurs réponses (réponses différentes en fonction utilisateur):
+
+eg. `requests/login.json`
+```json
+{
+  "get": {
+    "response": {
+      "jean": {
+
+      },
+      "marie": {
+
+      }
+    },
+    "action": ""
+  }
+  
+}
+```
+
+- "get", "put", "delete", "post": argument verbe
+  - response: la réponse pour "Jean" et "Marie"
+  - action: nom d'une fonction python pour modifier des valeurs temporaires (arguments possibles: `data`: request data)
+
+- Un fichier `requests.json` qui lie route et requête :
+```json
+{
+  "/login.awp": "login.json"
+}
+```
+
+## Contribuer
+
+- Corrigez des requêtes
+- Ajoutez des requêtes
+- Ou reporter un bug !
---
+++ main.py
@@ -0,0 +1,128 @@
+import json
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.datastructures import Headers
+
+app = FastAPI(title="EcoleDirecte test API")
+accounts = {
+    "avraidireonsenfoucestpourtestmaisvoicitontoken": {
+        "username": "jean",
+        "password": "jean%",
+        "id": 0
+    },
+    "marieaussiauntokenetellepeutseconnecteraedsuper": {
+        "username": "marie",
+        "password": "marie%",
+        "id": 1
+    }
+}
+
+requests_count = {
+    "avraidireonsenfoucestpourtestmaisvoicitontoken": 0,
+    "marieaussiauntokenetellepeutseconnecteraedsuper": 0
+}
+
+temp_data = {
+
+}
+
+
+def read_json(path: str):
+    return json.loads(open(path).read())
+
+
+def error_request(error: int):
+    if error == 505:
+        return {
+            "code": 505,
+            "token": "",
+            "host": "HTTP<ed-test-srv>",
+            "message": "Identifiant et/ou mot de passe invalide !",
+            "data": {
+                "accounts": []
+            }
+        }
+    elif error == 520:
+        return {
+            "code": 520,
+            "token": "",
+            "host": "HTTP<ed-test-srv>",
+            "message": "Token invalide",
+            "data": {
+                "accounts": []
+            }
+        }
+    elif error == 525:
+        return {
+            "code": 525,
+            "token": "",
+            "host": "HTTP<ed-test-srv>",
+            "message": "Token expiré",
+            "data": {
+                "accounts": []
+            }
+        }
+    elif error == 40129:
+        return {
+            "code": 40129,
+            "token": "",
+            "host": "HTTP<ed-test-srv>",
+            "message": "Format JSON invalide",
+            "data": {
+                "accounts": []
+            }
+        }
+    return {
+        "code": error,
+        "token": "",
+        "host": "HTTP<ed-test-srv>",
+        "message": "Erreur",
+        "data": {
+            "accounts": []
+        }
+    }
+
+
+def loginMiddleware(data: dict, headers: Headers, path: str):
+    if path == '/login.awp':
+        username = data['identifiant']
+        password = data['motdepasse']
+        for token, user in accounts.values():
+            if user['username'] == username:
+                if user['password'] == password:
+                    return True, username, token, 200
+        return False, '', '', 505
+    else:
+        token = headers.get('X-Token', '')
+        if token in accounts.keys():
+            if requests_count[token] > 10 and not conf['static_token']:
+                return False, '', '', 525
+            return True, accounts['token']['username'], '', 200
+        else:
+            return False, '', '', 520
+
+
+if __name__ == '__main__':
+    conf = read_json("config.json")
+    routes = read_json("requests.json")
+
+    # Register routes
+    for route in routes.keys():
+        schema = routes[route]
+
+        @app.post(route)
+        async def handler(request: Request, verbe: str = 'default', v: str = 'unknown'):
+            print(f"[{route}] Receiving \"{verbe}\" request, requesting version {v}")
+            body = await request.body()
+            data = json.loads(body.decode().replace('data=', '', 1))
+
+            is_logged_in, user, token, code = loginMiddleware(data, request.headers, route)
+
+            if is_logged_in:
+                response = schema[verbe]['response'][user]
+                response['token'] = token
+                return response
+            else:
+                return error_request(code)
+
+    uvicorn.run(app)
