babff798400814aaf454208dbf80c850185a7eb4
camarm-dev <contact@camarm.dev>
2024-04-18 20:18:24 +0200

    feat(auth): ajout de la procédure d'authentification par access token

--- server/routes/auth/login.py
+++ server/routes/auth/login.py
@@ -2,6 +2,7 @@ from fastapi import Request, Depends
 from sqlalchemy.orm import Session
 
 from server.main import app
+from server.models.accounts.student import Student
 from server.utils.auth import authenticate_user, generate_token, is_access_token_valid, authenticate_access_token, \
     generate_access_token
 from server.utils.database import get_database
@@ -10,10 +11,21 @@ from server.utils.response import response
 from server.errors import ERRORS
 
 
+def login_response(user: Student):
+    token = generate_token(user.id)
+    data = {
+        "accounts": [user]
+    }
+    return response(data, token, 200, "Connexion réussie !")
+
+
 async def re_login_procedure(body: dict, db: Session):
     access_token = body.get('accessToken', '')
     if is_access_token_valid(access_token):
-        user = await authenticate_access_token()
+        username = body.get('username', '')
+        user = await authenticate_access_token(username, access_token, db)
+        return login_response(user)
+    return ERRORS.INVALID_TOKEN
 
 
 async def login_procedure(body: dict, db: Session):
@@ -26,13 +38,9 @@ async def login_procedure(body: dict, db: Session):
     remember_me = body.get('sesouvenirdemoi', False)
 
     if user:
-        token = generate_token(user.id)
         if uuid and remember_me:
             user['accessToken'] = generate_access_token(uuid)
-        data = {
-            "accounts": [user]
-        }
-        return response(data, token, 200, "Connexion réussie !")
+        return login_response(user)
     return ERRORS.INVALID_CREDS
 
 
